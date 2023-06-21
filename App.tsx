772d946ae743ae347c20390b8c2376c587c70ef7
Jakob Willforss <jakob.willforss@hotmail.com>
2023-06-21 15:11:14 +0200

    Minimal checklist example running

--- App.tsx
+++ App.tsx
@@ -5,94 +5,68 @@
  * @format
  */
 
-import React from 'react';
 import type {PropsWithChildren} from 'react';
+import React, {useState} from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
+  Button,
+  FlatList,
   StyleSheet,
   Text,
-  useColorScheme,
+  TextInput,
   View,
 } from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
 
 type SectionProps = PropsWithChildren<{
   title: string;
 }>;
 
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
+interface Task {
+  id: string;
+  title: string;
 }
 
 function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+  const [task, setTask] = useState('');
+  const [tasks, setTasks] = useState<Task[]>([]);
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  const handleAddTask = () => {
+    console.log(task);
+    if (task !== '') {
+      const newTask: Task = {
+        id: String(Date.now()),
+        title: task,
+      };
+      setTasks([...tasks, newTask]);
+      setTask('');
+    }
+  };
+
+  const handleRemoveTask = (id: string) => {
+    const updatedTasks = tasks.filter(task => task.id !== id);
+    setTasks(updatedTasks);
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <View>
+      <Text>Checklist</Text>
+      <View>
+        <TextInput
+          placeholder="Enter..."
+          value={task}
+          onChangeText={text => setTask(text)}></TextInput>
+      </View>
+      <Button title="Add" onPress={handleAddTask}></Button>
+      <FlatList
+        data={tasks}
+        renderItem={({item}) => (
+          <View>
+            <Text>{item.title}</Text>
+            <Button
+              title="Remove"
+              onPress={() => handleRemoveTask(item.id)}></Button>
+          </View>
+        )}></FlatList>
+    </View>
   );
 }
 
