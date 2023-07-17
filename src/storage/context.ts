5def475cda2226eeb735ebda5c9c3923ea10b1ea
Jakob Willforss <jakob.willforss@hotmail.com>
2023-07-17 21:00:03 +0200

    Add task interace

--- src/storage/context.ts
+++ src/storage/context.ts
@@ -1,5 +1,10 @@
 import { createContext } from 'react'
 
+interface Task {
+  id: string
+  label: string
+}
+
 const StorageContext = createContext<{
   entries: Task[]
   saveEntries: (entries: Task[]) => void
