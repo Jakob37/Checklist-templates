b16913599021d1b2a326ca2023db06e382f6031a
Jakob Willforss <jakob.willforss@hotmail.com>
2023-07-26 20:57:06 +0200

    Toggle checks through immutable data

--- src/screens/checklists.tsx
+++ src/screens/checklists.tsx
@@ -4,29 +4,69 @@ import { useContext } from 'react'
 import { CheckboxStatus } from '../storage/interfaces'
 import { IconButton } from '../views/iconbutton'
 import { icons } from '../ux/icons'
+import { assert } from '../util/util'
 
 function Checklists() {
-  const { checklists } = useContext(StorageContext)
+  const { checklists, saveChecklists } = useContext(StorageContext)
+
+  function toggleCheck(checklistId: string, checkboxId: string) {
+    const targetChecklist = checklists.filter(
+      checklist => checklist.id === checklistId,
+    )
+    assert(targetChecklist.length === 1, 'One checklist expected')
+
+    const updatedChecklist = { ...targetChecklist[0] }
+    updatedChecklist.checkboxes = targetChecklist[0].checkboxes.map(
+      checkbox => {
+        if (checkbox.id === checkboxId) {
+          const newChecked =
+            checkbox.checked === CheckboxStatus.checked
+              ? CheckboxStatus.unchecked
+              : CheckboxStatus.checked
+          const newCheckbox = { ...checkbox }
+          newCheckbox.checked = newChecked
+          return newCheckbox
+        } else {
+          return checkbox
+        }
+      },
+    )
+
+    saveChecklists([
+      ...checklists.filter(checklist => checklist.id !== checklistId),
+      updatedChecklist,
+    ])
+  }
 
   return (
     <View>
       <Text>Ongoing checklists</Text>
       {checklists.map((checklist, i) => (
         <View key={checklist.id}>
-          <Text style={{ fontWeight: 'bold' }}>{checklist.template.label}</Text>
+          <IconButton
+            onPress={() => {
+              // FIXME: Generalize with the template logic
+              const retainedChecklists = checklists.filter(
+                c => c.id !== checklist.id,
+              )
+              assert(
+                retainedChecklists.length === checklists.length - 1,
+                `One template less expected after removal`,
+              )
+              saveChecklists(retainedChecklists)
+            }}
+            icon={icons.trash}
+            label={checklist.template.label}></IconButton>
           {checklist.checkboxes.map(checkbox => {
+            console.log(checkbox)
             return (
-              <View key={checkbox.id} style={{ flexDirection: 'row' }}>
+              <View key={`${checkbox.id}`} style={{ flexDirection: 'row' }}>
                 <IconButton
                   onPress={() => {
-                    if (checkbox.checked === 1) {
-                      checkbox.checked = 2
-                    } else {
-                      checkbox.checked = 1
-                    }
+                    toggleCheck(checklist.id, checkbox.id)
                   }}
                   icon={checkbox.checked === 1 ? icons.check : icons.uncheck}
-                  label={checkbox.label}></IconButton>
+                  label={`${checkbox.label} (checked ${checkbox.checked})`}></IconButton>
               </View>
             )
           })}
