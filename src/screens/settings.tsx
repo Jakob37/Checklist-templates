b4111bda71aa6cf21e2429e6b3df3d8da26565c3
Jakob Willforss <jakob.willforss@hotmail.com>
2023-08-06 20:22:18 +0200

    Draft JSON export function

--- src/screens/settings.tsx
+++ src/screens/settings.tsx
@@ -1,7 +1,8 @@
-import { Button, View } from 'react-native'
+import { Alert, Button, Platform, View } from 'react-native'
 import { ds } from '../ux/design'
 import { useContext } from 'react'
 import { StorageContext } from '../storage/context'
+import RNFS from 'react-native-fs'
 
 function Settings() {
   const { checklists, templates } = useContext(StorageContext)
@@ -23,11 +24,45 @@ function Settings() {
         <Button
           onPress={() => {
             console.log('Export')
+            writeJSON('Hello world', 'filename')
           }}
           title="Export JSON"></Button>
       </View>
     </View>
   )
+}
+
+async function writeJSON(dataString: string, name: string) {
+  try {
+    const filePath = `${RNFS.DocumentDirectoryPath}/${name}.json`
+    await RNFS.writeFile(filePath, dataString)
+
+    if (Platform.OS === 'android' && RNFS.ExternalStorageDirectoryPath) {
+      const destPath = `${RNFS.ExternalStorageDirectoryPath}/Download/${name}.json`
+      await RNFS.moveFile(filePath, destPath)
+      Alert.alert(
+        'Success',
+        'JSON file generated and exported to the Downloads folder!',
+      )
+    } else if (Platform.OS === 'ios') {
+      const destPath = `${RNFS.DocumentDirectoryPath}/${name}.json`
+      await RNFS.moveFile(filePath, destPath)
+      Alert.alert(
+        'Success',
+        'JSON file generated and exported to the Documents directory',
+      )
+    } else {
+      Alert.alert(
+        'Warning',
+        'Exporting to Downloads folder is not supported on this platform',
+      )
+    }
+
+    Alert.alert('Success', 'JSON file generated and written')
+  } catch (error) {
+    console.error('Error downloading JSON file', error)
+    Alert.alert('Error', 'An error occured while downloading the JSON file')
+  }
 }
 
 export default Settings
